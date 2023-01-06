68dd7bd1928537e3e20fa21bb9a7406cdc9d04f9
moqie <s3mpau@gmail.com>
2023-01-06 15:36:32 +0300

    added two methods

---
+++ Controller/PhotorazeController.py
@@ -0,0 +1,7 @@
+from Model.PhotorazeModel import ImageModel
+
+URL = '../images/1.jpg'
+img = ImageModel(URL, 'baza.jpg', ['paper', 'info'])
+
+img.save()
+img.delete()
--- Model/PhotorazeModel.py
+++ Model/PhotorazeModel.py
@@ -1,17 +1,27 @@
-STORAGE_URL = '/storage'
+import os
+
+from PIL import Image
+
+STORAGE_URL = '../storage/'
 
 
-class Image:
+class ImageModel:
     def __init__(self, url, name, tags):
         self.url = url
         self.name = name
         self.tags = tags
+        self.img = Image.open(self.url)
 
     def save(self):
+        self.img.save(STORAGE_URL + self.name)
         pass
+
+    def delete(self):
+        os.remove(STORAGE_URL + self.name)
+        pass
+
     def get_by_tag(self):
         pass
+
     def set_tag(self):
-        pass
-    def delete(self):
         pass
